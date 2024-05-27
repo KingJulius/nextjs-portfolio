32fb19b87ec34b5c747880ce3228873c86a075f1
Julius Dsouza <jsdsz1996@gmail.com>
2024-05-27 01:32:05 -0700

    Added Intro Section (#2)
    * Added assets & updated config
    
    * Added data and json files
    
    * Added intro-section ui

---
+++ data/constants.ts
@@ -0,0 +1,1 @@
+export const MY_EMAIL = 'jsdsz199^@gmail.com'
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./data/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   prefix: "",
   theme: {
